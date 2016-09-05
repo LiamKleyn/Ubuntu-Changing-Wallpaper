4942a0427eae3a44b166efc1277c5a3e09cef4d7
LiamKleyn <lhkleyn@gmail.com>
2016-09-05 09:44:26 +0200

    Custom URL working now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import urllib
+import os
 
 
 print ("Hi, this script by Liam Kleyn will change your wallpaper to a random image from unsplash.it every x minutes, which you can specify, as well as the resolution!")
@@ -10,8 +11,14 @@ usertime = raw_input("Please enter background change intervals in minutes")
 print ("Resolution is,", userreswidth, userresheight)
 print ("Interval is,", usertime)
 
-url = 'https://unsplash.it/%s/%r/?random' % userreswidth,userresheight
+
+
+
+url = 'https://unsplash.it/' + userreswidth + '/' + userresheight + '/?random'
+print url
 resource = urllib.urlopen(url)
-output = open("file01.jpg","wb")
+output = open("background.jpg","wb")
 output.write(resource.read())
 output.close()
+
+os.system("gsettings set org.gnome.desktop.background picture-uri file:/home/liam/tempclones/Ubuntu-Changing-Wallpaper/background.jpg")
