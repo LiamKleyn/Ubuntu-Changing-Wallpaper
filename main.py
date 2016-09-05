f155a3fff5319b90ec3b5798ee8c34122bc60b15
LiamKleyn <lhkleyn@gmail.com>
2016-09-05 08:55:57 +0200

    Second commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-person = input('Enter your name: ')
-print('Hello', person)
-print ("test test test")
+print ("Hi, this script by Liam Kleyn will change your wallpaper to a random image from unsplash.it every x minutes, which you can specify, as well as the resolution!")
+userreswidth = raw_input("Please enter your resolution width: ")
+userresheight = raw_input("Please enter your resolution height: ")
+
+usertime = raw_input("Please enter background change intervals in minutes")
+
+print ("Resolution is,", userreswidth, userresheight)
+print ("Interval is,", usertime)
+
+wget -O ~/background.jpg https://unsplash.it/$userreswidth/userresheight/?random
+
+/usr/bin/gsettings set org.gnome.desktop.background picture-uri "file:///home/liam/background.jpg"
