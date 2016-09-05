ce277d3d46f1e39d7befb9c94a935553f209cc8c
LiamKleyn <lhkleyn@gmail.com>
2016-09-05 09:20:00 +0200

    3rd

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import urllib
+
+
 print ("Hi, this script by Liam Kleyn will change your wallpaper to a random image from unsplash.it every x minutes, which you can specify, as well as the resolution!")
 userreswidth = raw_input("Please enter your resolution width: ")
 userresheight = raw_input("Please enter your resolution height: ")
@@ -7,6 +10,8 @@ usertime = raw_input("Please enter background change intervals in minutes")
 print ("Resolution is,", userreswidth, userresheight)
 print ("Interval is,", usertime)
 
-wget -O ~/background.jpg https://unsplash.it/$userreswidth/userresheight/?random
-
-/usr/bin/gsettings set org.gnome.desktop.background picture-uri "file:///home/liam/background.jpg"
+url = 'https://unsplash.it/%s/%r/?random' % userreswidth,userresheight
+resource = urllib.urlopen(url)
+output = open("file01.jpg","wb")
+output.write(resource.read())
+output.close()
